215b2e7ceb587dc52c1eb267efa5cb19339176d1
Pyae Hmoo Maung <lunasky.95@gmail.com>
2020-11-01 21:21:58 +0630

    integrated firebase into application,created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 import HomePage from './page/homepage/homepage.componet';
 import ShopPage from './page/shop/shop.component.jsx';
-
-function App () {
-	return (
+import Header from './component/header/header.component.jsx';
+import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import {auth } from './firebase/firebase.utils';
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state ={
+			currentUser:null
+		}
+	}
+	unsubscirbeFormAuth =null;
+	componentDidMount(){
+		this.unsubscirbeFormAuth=auth.onAuthStateChanged(user=>{
+			this.setState({ currentUser:user});
+			console.log(user);
+		});
+	}
+	componentWillUnmount(){
+		this.unsubscirbeFormAuth();
+	}
+	render(){
+		return(
 		<div>
+		<Header currentUser={this.state.currentUser}/>
+		<Switch>
 			<Route exact path='/' component={HomePage} />
 			<Route path='/shop' component={ShopPage}/>
+			<Route path='/signin' component={SignInAndSignUpPage}/>
+		</Switch>
 		</div>
 	);
+	}
 }
-
+	
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firebase-auth';
+import 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyAW26J3w8q22zfwxkjF1aOfRVhFetU99pw",
+    authDomain: "crwn-db-bd6cd.firebaseapp.com",
+    databaseURL: "https://crwn-db-bd6cd.firebaseio.com",
+    projectId: "crwn-db-bd6cd",
+    storageBucket: "crwn-db-bd6cd.appspot.com",
+    messagingSenderId: "43710246251",
+    appId: "1:43710246251:web:baf55690464e86be7ab02e",
+    measurementId: "G-B3P05CLRSE"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  export const auth=firebase.auth();
+  export const firestore =firebase.firestore();
+
+  const provider=new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt:'select_account'});
+  export const signInwithGoogle=()=>auth.signInWithPopup(provider);
+  export default firebase;
