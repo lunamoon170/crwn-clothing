0f5be4dd553f32c6626e05299406e51dd08afbd4
Pyae Hmoo Maung <lunasky.95@gmail.com>
2020-11-04 12:29:59 +0630

    added redux , and implemented userReducer and userActions to headers and app components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import {connect} from 'react-redux';
 import HomePage from './page/homepage/homepage.componet';
 import ShopPage from './page/shop/shop.component.jsx';
 import Header from './component/header/header.component.jsx';
 import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.action';
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			currentUser: null,
-		};
-	}
+	
 	unsubscirbeFormAuth = null;
 	componentDidMount() {
+		const {setCurrentUser}=this.props;
 		this.unsubscirbeFormAuth = auth.onAuthStateChanged(async (userAuth) => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 				userRef.onSnapshot((snapShot) => {
-					this.setState({
-						currentUser: {
+					setCurrentUser({
 							id: snapShot.id,
 							...snapShot.data(),
-						},
-					});
+						})
 				});
 			}
-			this.setState({ currentUser: userAuth });
+			setCurrentUser( userAuth );
 		});
 	}
 	componentWillUnmount() {
@@ -36,7 +32,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header currentUser={this.state.currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
@@ -47,4 +43,7 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapDispatchProps = dispatch => ({
+	setCurrentUser:user=>dispatch(setCurrentUser(user))
+});
+export default connect(null,mapDispatchProps)(App);
